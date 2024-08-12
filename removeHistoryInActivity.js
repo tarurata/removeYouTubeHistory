cccf3195a0519ab2f7223f09d7de8983f8b4e6ec
tarurata <saycheese5963@gmail.com>
2024-08-12 11:17:57 -0700

    Merged to activity page

--- removeHistoryInActivity.js
+++ removeHistoryInActivity.js
@@ -1,31 +1,31 @@
-// Use on this URL: https://myactivity.google.com/product/youtube?hl=en&utm_medium=web&utm_source=youtube
-let searchHistoryElements = document.querySelectorAll("div.k2bP7e.YYajNd");
-let japaneseRegex = /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FBF]/;
+// URL: https://myactivity.google.com/product/youtube
+function removeJapaneseSearchHistory() {
+    let activityCards = document.querySelectorAll("c-wiz");
+    let japaneseRegex = /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FBF]/;
 
-searchHistoryElements.forEach(element => {
-    if (!element.textContent.includes("Searched for")) {
-        return;
-    }
+    activityCards.forEach(element => {
+        // Check if the card is a YouTube activity card (including the search history)
+        if (!element.firstElementChild.getAttribute("aria-label")) { return; }
+        if (!element.firstElementChild.getAttribute("aria-label").includes("Card showing an activity from YouTube")) { return; }
 
-    let linkElement = element.querySelector("a");
-    if (!linkElement) {
-        return;
-    }
+        let videoText = element.textContent;
+        // Check if the text contains Japanese characters
+        if (!japaneseRegex.test(videoText)) {
+            console.log("Could not find Japanese...");
+            return;
+        }
 
-    let videoText = element.textContent;
-    // Check if the text contains Japanese characters
-    if (!japaneseRegex.test(videoText)) {
-        console.log("Could not find Japanese...");
-        return;
-    }
+        console.log(`Found Japanese search: ${videoText}`);
+        let removeButton = element.querySelector("button");
 
-    console.log(`Found Japanese search: ${videoText}`);
-    let removeButton = element.querySelector("button");
+        if (removeButton) {
+            removeButton.click();
+            console.log(`${videoText} Removed!`);
+        } else {
+            console.log(`${videoText} couldn't be removed...`);
+        }
+    });
+}
 
-    if (removeButton) {
-        removeButton.click();
-        console.log(`${videoText} Removed!`);
-    } else {
-        console.log(`${videoText} couldn't be removed...`);
-    }
-});
+setInterval(removeJapaneseSearchHistory, 5000);
+removeJapaneseSearchHistory();
